fd02fd873bf5a1128d7354d30eb197cdf6e6cc81
Eugene Levinson <eugene.levinson@tailify.com>
2023-01-06 23:39:32 +0000

    hello world web page

--- app/app.ts
+++ app/app.ts
@@ -1,3 +1,19 @@
-var greet: string = "Greetings"; 
-var geeks: string = "lololo"; 
-console.log(greet + " from " + geeks); 
+import express from "express";
+import path from "path";
+
+const app = express();
+
+
+// Index Page
+app.get('/', function(req, res) {
+    res.status(200)
+    res.send('hello world');
+});
+
+// Set static dir for static resources
+app.use('/assets', express.static(path.join(__dirname + '/static/assets')))
+
+
+app.listen(80, () => {     
+    console.log( `server started at http://localhost:${80}`);
+});
