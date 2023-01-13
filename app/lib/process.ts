50c6124bb3b1a2835afd8a2e96cefb0d7101e379
Eugene Levinson <eugene.levinson@tailify.com>
2023-01-13 00:36:23 +0000

    fixed relative stress calculation bug

--- app/lib/process.ts
+++ app/lib/process.ts
@@ -1,16 +1,15 @@
 export function calculatePercentageIncrease(data: number[], xSmallest: number, xLargest: number): number {
-
-    console.log(data)
+    const dataToSort = data
 
     // Sort the data in ascending order
-    data.sort((a, b) => a - b);
+    dataToSort.sort((a, b) => a - b);
 
     const smallestValues = data.slice(0, xSmallest);
     const largestValues = data.slice(-xLargest);
     const avgSmallest = smallestValues.reduce((a, b) => a + b) / xSmallest;
     const avgLargest = largestValues.reduce((a, b) => a + b) / xLargest;
-  
-    const percentageIncrease = ((avgLargest - avgSmallest) / avgLargest);
 
-    return percentageIncrease;
+    const diff = avgLargest - avgSmallest
+
+    return diff
 }
