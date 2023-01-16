6ecbb8f8d02609b3574fdca1b5319c839158f1d2
Eugene Levinson <el769@bath.ac.uk>
2023-01-16 14:27:43 +0000

    added ssl

--- app/app.ts
+++ app/app.ts
@@ -5,6 +5,10 @@ import {config} from "./config"
 import routes from './routers/web';
 import api_routes from './routers/api';
 
+var fs = require('fs');
+var http = require('http');
+var https = require('https');
+
 const app = express();
 
 app.use(express.json());
@@ -28,12 +32,45 @@ app.use(function(req, res, next) {
     } else {
       next();
     }
+});
+
+
+
+var http_server: any;
+var httpsServer: any;
+
+if (config.ssl_on){
+  var privateKey  = fs.readFileSync(config.ssl_key, 'utf8');
+  var certificate = fs.readFileSync(config.ssl_cert, 'utf8');
+
+  var credentials = {key: privateKey, cert: certificate};
+
+  // Create https server
+  httpsServer = https.createServer(credentials, app);
+  httpsServer.listen(config.https_port);
+
+  console.log( `Server started at https://localhost:${config.https_port}`);
+
+
+  // Creating an http server
+  http_server = http.createServer((req:any, res:any) => {
+    var host = req.headers.host
+    var redirect_url = `https://${host}${req.url}`;
+    res.writeHead(301,{Location: redirect_url});
+    res.end();
   });
 
+} else {
 
+
+  http_server = http.createServer(app);
+  console.warn("HTTPS is Off. Only HTTP server is running!")
+
+
+}
 
 
 // Http webserver start
 app.listen(config.http_port, () => {     
-    console.log( `server started at http://localhost:${config.http_port}`);
+    console.log( `Server started at http://localhost:${config.http_port}`);
 });
--- app/config.ts
+++ app/config.ts
@@ -5,7 +5,10 @@ export interface Config {
     db_user: string,
     db_pass: string
     db_name: string,
-    db_port: number
+    db_port: number,
+    ssl_on: boolean | unknown,
+    ssl_key: string,
+    ssl_cert: string
 }
 
 export const config: Config = {
@@ -15,6 +18,9 @@ export const config: Config = {
     db_user: "postgres",
     db_pass: "pg-admin",
     db_name: "postgres",
-    db_port: 5432
+    db_port: 5432,
+    ssl_on: process.env.SSL_ON,
+    ssl_key: "/var/ssl/key.pem",
+    ssl_cert: "/var/ssl/cert.pem"
 
 }
